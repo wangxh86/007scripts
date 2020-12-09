d6655e9baa9cbc575a7b28d3e27da26052dea313
jpcnmm <jpcnmm@gmail.com>
2020-12-09 14:40:24 +0800

    修复body

--- qqTIMEHD.js
+++ qqTIMEHD.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let QQ_TIMEHDs = [
-  '{"ywsession":"oib0j0e9ejcishqo4s8624jziwt4qz40","Cookie":"ywguid=61718369;ywkey=ywS7ZeLF6V9v;platform=ios;channel=mqqmina;mpVersion=0.31.0;qq_ver=8.2.8;os_ver=iOS 14.2;mpos_ver=1.12.1;platform=ios;openid=514CA3BE822B3986DE4586AC10895E9E","Connection":"keep-alive","Content-Type":"application/json","Accept":"*/*","Host":"mqqapi.reader.qq.com","User-Agent":"QQ/8.2.8.633 CFNetwork/1206 Darwin/20.1.0","Referer":"https://appservice.qq.com/1110657249/0.31.0/page-frame.html","Accept-Language":"zh-cn","Accept-Encoding":"gzip, deflate, br","mpversion":"0.31.0"}',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  '{"ywsession":"3t02l6znzh8mq3egoxvzq8kzrg1ese0o","Cookie":"ywguid=61718369;ywkey=ywCacnJRBAZB;platform=ios;channel=mqqmina;mpVersion=0.32.5;qq_ver=8.4.17;os_ver=iOS 14.2;mpos_ver=1.21.0;platform=ios;openid=514CA3BE822B3986DE4586AC10895E9E","Connection":"keep-alive","Content-Type":"application/json","Accept":"*/*","Host":"mqqapi.reader.qq.com","User-Agent":"QQ/8.4.17.638 CFNetwork/1206 Darwin/20.1.0","Referer":"https://appservice.qq.com/1110657249/0.32.5/page-frame.html","Accept-Language":"zh-cn","Accept-Encoding":"gzip, deflate, br","mpversion":"0.32.5"}',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   //账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
@@ -20,8 +20,8 @@ if (process.env.QQREAD_TIMEHD) {
     QQ_TIMEHDs = [process.env.QQREAD_TIMEHD];
   }
   QQ_TIMEHDs = [...new Set(QQ_TIMEHDs)]
-  console.log(`\n====================共有${QQ_TIMEHDs.length}个企鹅账号Cookie=========\n`);
-  console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
+  //console.log(`\n====================共有${QQ_TIMEHDs.length}个企鹅账号Cookie=========\n`);
+  //console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
   // console.log(`\n==================脚本执行来自 github action=====================\n`)
 }
 for (let i = 0; i < QQ_TIMEHDs.length; i++) {
--- qqTIMEURL.js
+++ qqTIMEURL.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let QQ_TIMEURLs = [
-  'https://mqqapi.reader.qq.com/mqq/addReadTimeWithBid?scene=3001&refer=pages%2Fbook-shelf%2Findex&bid=13360762&readTime=9310&read_type=0&conttype=1&read_status=0&chapter_info=%5B%7B%221%22%3A%7B%22readTime%22%3A9310%2C%22pay_status%22%3A0%7D%7D%5D&sp=-1',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'https://mqqapi.reader.qq.com/mqq/addReadTimeWithBid?scene=2016&refer=pages%2Findex%2Findex&bid=20506956&readTime=6816&read_type=0&conttype=1&read_status=0&chapter_info=%5B%7B%221%22%3A%7B%22readTime%22%3A6816%2C%22pay_status%22%3A0%7D%7D%5D&sp=-1',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   //账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
@@ -20,8 +20,8 @@ if (process.env.QQREAD_TIMEURL) {
     QQ_TIMEURLs = [process.env.QQREAD_TIMEURL];
   }
   QQ_TIMEURLs = [...new Set(QQ_TIMEURLs)]
-  console.log(`\n====================共有${QQ_TIMEURLs.length}个企鹅账号Cookie=========\n`);
-  console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
+  //console.log(`\n====================共有${QQ_TIMEURLs.length}个企鹅账号Cookie=========\n`);
+  //console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
   // console.log(`\n==================脚本执行来自 github action=====================\n`)
 }
 for (let i = 0; i < QQ_TIMEURLs.length; i++) {
