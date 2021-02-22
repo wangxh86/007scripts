22558f96a1569b2ccbf355d573500a5c6aa24f71
jpcnmm <jpcnmm@gmail.com>
2021-02-22 09:36:38 +0800

    Update txstockCOOKIE.js

--- Task/txstockCOOKIE.js
+++ Task/txstockCOOKIE.js
@@ -1,19 +1,19 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let userheaderVal= `oA0GbjpHujezy-GAgJXJPkRUGlvs&_appName=ios&_dev=iPhone11,8&_devId=6640a3384872fecf13f652b28bd6e5783773130e&_appver=8.8.1&_ifChId=&_isChId=1&_osVer=14.3&openid=oA0GbjpHujezy-GAgJXJPkRUGlvs&fskey=v0aaf63c222602f59838fbc86d9192ff&appid=wxcbc3ab3807acb685&access_token=42_PVkMIZ5j2HtEfmDBDR1fGG4cg7kelpt4fgQg6Avi0kkGgzygR3vzZaMQNaaXSKnt9vSLpTn5aVmQZ0dkhv-HfmgTLAkfBW6UtcppN8D1-Pg&buildType=store&check=11&_idfa=00000000-0000-0000-0000-000000000000&lang=zh_CN`
+let userheaderVal= ``
 
-let userkeyVal= `pgv_pvid=5853813260`
+let userkeyVal= ``
 
-let cashheaderVal= `oA0GbjpHujezy-GAgJXJPkRUGlvs&fskey=v0aaf63c222602f59838fbc86d9192ff&access_token=42_PVkMIZ5j2HtEfmDBDR1fGG4cg7kelpt4fgQg6Avi0kkGgzygR3vzZaMQNaaXSKnt9vSLpTn5aVmQZ0dkhv-HfmgTLAkfBW6UtcppN8D1-Pg&_appName=ios&_appver=8.8.1&_osVer=14.3&_devId=6640a3384872fecf13f652b28bd6e5783773130e`
+let cashheaderVal= ``
 
-let signheaderVal= `oA0GbjpHujezy-GAgJXJPkRUGlvs&fskey=v0aaf63c222602f59838fbc86d9192ff&channel=1&access_token=42_PVkMIZ5j2HtEfmDBDR1fGG4cg7kelpt4fgQg6Avi0kkGgzygR3vzZaMQNaaXSKnt9vSLpTn5aVmQZ0dkhv-HfmgTLAkfBW6UtcppN8D1-Pg&_appName=ios&_appver=8.8.1&_osVer=14.3&_devId=6640a3384872fecf13f652b28bd6e5783773130e`
+let signheaderVal= ``
 
-let signkeyVal= `pgv_pvid=5853813260; ts_last=/activity/page/activityForward/; ts_refer=zqact01.tenpay.com/activity/page/appNewcomerSpree/; ts_sid=2819936403; ts_uid=8237901996`
+let signkeyVal= ``
 
-let taskheaderVal= `&_dev=iPhone11,8&_devId=6640a3384872fecf13f652b28bd6e5783773130e&_appver=8.8.1&_ifChId=&_isChId=1&_osVer=14.3&openid=oA0GbjpHujezy-GAgJXJPkRUGlvs&fskey=v0aaf63c222602f59838fbc86d9192ff&appid=wxcbc3ab3807acb685&access_token=42_PVkMIZ5j2HtEfmDBDR1fGG4cg7kelpt4fgQg6Avi0kkGgzygR3vzZaMQNaaXSKnt9vSLpTn5aVmQZ0dkhv-HfmgTLAkfBW6UtcppN8D1-Pg&buildType=store&check=11&_idfa=00000000-0000-0000-0000-000000000000&lang=zh_CN`
+let taskheaderVal= ``
 
-let taskkeyVal= `pgv_info=ssid=s9855562537; pgv_pvid=5853813260`
+let taskkeyVal= ``
 
-let wxtaskkeyVal= `wzq_channel=fm_wzq_wx_v1_unknow_01..; qlappid=wx9cf8c670ebd68ce4; qlskey=v6474a41e11602f5ba2745f1df1c9c01; qluin=085e9858ef6105427a4552ccb@wx.tenpay.com; qq_logtype=16; wx_session_time=1613716386000; wzq_qlappid=wx9cf8c670ebd68ce4; wzq_qlskey=v6474a41e11602f5ba2745f1df1c9c01; wzq_qluin=os-ppuHDln1oa1RzHrRwd6eNmwNA`
+let wxtaskkeyVal= ``
 
 
 let cookieArr = {
