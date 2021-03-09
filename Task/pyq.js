fa3490082c6ece72d871ec3c9345879de52db33c
jpcnmm <jpcnmm@gmail.com>
2021-03-09 12:16:40 +0800

    Update pyq.js

--- Task/pyq.js
+++ Task/pyq.js
@@ -176,7 +176,7 @@ let uid = pyqad.match(/\d{6}/)
     	}
    $.post(qd_url,async(error, response, data) =>{
     try{
-       data=data.replace(/^.*{/, `{`)
+       data=data.replace(/^.*?{/, `{`)
        $.log(data)
        const result = JSON.parse(data)
         if(logs)$.log(data)
@@ -209,7 +209,7 @@ $.log('点赞图文id为：'+id)
     	}
    $.post(tp_url,async(error, response, data) =>{
     try{
-       data=data.replace(/^.*{/, `{`) 
+       data=data.replace(/^.*?{/, `{`) 
        const result = JSON.parse(data)
         if(logs)$.log(data)
         console.log(result.msg+'\n')
@@ -241,7 +241,7 @@ let uid = pyqad.match(/\d{6}/)
     	}
    $.post(tp_d_url,async(error, response, data) =>{
     try{
-       data=data.replace(/^.*{/, `{`) 
+       data=data.replace(/^.*?{/, `{`) 
        const result = JSON.parse(data)
         if(logs)$.log(data)
         console.log(result.msg+'\n')
@@ -278,9 +278,9 @@ ${uid}
     	}
    $.post(comment_url,async(error, response, data) =>{
     try{
-       $.log(data)
+       //$.log(data)
        data=data.replace(/^.*?{/, `{`) 
-       $.log(data)
+       //$.log(data)
        const result = JSON.parse(data)
         if(logs)$.log(data)
         console.log(result.msg+'\n')
@@ -312,9 +312,9 @@ let uid = pyqad.match(/\d{6}/)
     	}
    $.post(comment_list_url,async(error, response, data) =>{
     try{
-       $.log(data)
+       //$.log(data)
        data=data.replace(/^.*?{/, `{`) 
-       $.log(data)
+       //$.log(data)
        const result = JSON.parse(data)
         if(logs)$.log(data)
         let commentArr = result.list.find(item => item.uid == uid)
@@ -350,7 +350,7 @@ let uid = pyqad.match(/\d{6}/)
     	}
    $.post(commentdel_url,async(error, response, data) =>{
     try{
-       data=data.replace(/^.*{/, `{`) 
+       data=data.replace(/^.*?{/, `{`) 
        const result = JSON.parse(data)
         if(logs)$.log(data)
         console.log('评论'+result.msg+'\n')
@@ -381,7 +381,7 @@ let uid = pyqad.match(/\d{6}/)
     	}
    $.post(fx_url,async(error, response, data) =>{
     try{
-       data=data.replace(/^.*{/, `{`) 
+       data=data.replace(/^.*?{/, `{`) 
        const result = JSON.parse(data)
         if(logs)$.log(data)
         console.log('分享'+result.msg+'\n')
@@ -411,7 +411,7 @@ async function ad(){
     	}
    $.post(ad_url,async(error, response, data) =>{
     try{
-       data=data.replace(/^.*{/, `{`) 
+       data=data.replace(/^.*?{/, `{`) 
        const result = JSON.parse(data)
         if(logs)$.log(data)
         console.log('广告'+result.msg+'\n')
