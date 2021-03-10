a864db07cc3d0fbb9313830104dc4a8b410fb3eb
jpcnmm <jpcnmm@gmail.com>
2021-03-10 10:20:14 +0800

    Update hldsp.js

--- Task/hldsp.js
+++ Task/hldsp.js
@@ -115,9 +115,10 @@ if (
           hldsphd = hldsphdArr[i];
           $.index = i + 1;
           console.log(`\n开始【哈喽短视频${$.index}】`)
-
-            await hldspqd();
-	await hldsphhb();
+	//await hldsphhb();
+          await hldspqd();
+	  await $.wait(2000);  
+	  await hldspsp();
             
   }
 }}
@@ -199,11 +200,11 @@ let url = {
         console.log('\n哈喽短视频[签到]回执:成功🌝  \n获得金币:'+result.data.price)
      //$.done()
        await $.wait(2000);
-        await hldsprw();
+       //await hldsprw();
         
 } else {
 console.log('哈喽短视频[签到]回执:失败🚫 '+result.message)
-     await hldspsp();
+     //await hldspsp();
 }
         } catch (e) {
           //$.logErr(e, resp);
