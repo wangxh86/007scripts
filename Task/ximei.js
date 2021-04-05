8b7419e523c1b81e60d56f5548918d1ff72e7d0f
jpcnmm <jpcnmm@gmail.com>
2021-04-05 16:18:55 +0800

    Update ximei.js

--- Task/ximei.js
+++ Task/ximei.js
@@ -154,7 +154,7 @@ $.log(ximeihd)
 function ximei1(timeout = 0) {
   return new Promise((resolve) => {
     setTimeout( ()=>{
-      if (typeof $.getdata('ximeihd') === "undefined") {
+      if (typeof ximeihd === "undefined") {
         $.msg($.name,"",'请先获取西梅数据!😓',)
         $.done()
       }
